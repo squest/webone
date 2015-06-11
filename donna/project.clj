4facb5b355404b8eba75bebe888b726a3c6e9708
donnitazen <donna@zeniuseducation.com>
2015-06-11 21:33:42 +0700

    DB Session 7
    Nyoba DB dan pakai for. Ini pre Compojure

--- donna/project.clj
+++ donna/project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [zenedu.squest/questdb "0.2.2"]])
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [zenedu.squest/questdb "0.2.2"]
+                 [compojure "1.3.4"]
+                 [http-kit "2.1.16"]])
 
 
 
--- donna/src/donna/core.clj
+++ donna/src/donna/core.clj
@@ -1,4 +1,32 @@
 (ns donna.core
-    (:require [questdb.core :refer :all]))
+    (:require [questdb.core :refer :all]
+              [compojure.core :refer :all]
+              [compojure.route :as route]
+              [org.httpkit.server :refer [run-server]]))
 
-(defn db "firstdb")
+(defroutes app-routes
+           (GET "/" [] "Hello, Excellence!")
+           (GET "/westeros" [] "Hello, Westeros!")
+           (GET "/essos" [] "Hello, Essos!"))
+
+(defn -main []
+  (run-server myapp {:port 5000}))
+
+
+(def db "firstdb")
+
+(for [i (get-docs db)
+      j [{:homeland :Westeros}]
+      :let [gabung (put-doc! db (merge (get-doc db (i :uuid)) j))]]
+    gabung)
+
+(def lannbroh [{:name "Tywin Lannister" :status :deceased :description "killed by a dwarf" :homeland :Westeros :last_seen "King's Landing"}
+               {:name "Cersei Lannister" :status :alive :description "imprisoned by a sparrow" :homeland :Westeros :last_seen "Sacred Sept"}
+               {:name "Jamie Lannister" :status :alive :description "drinking with Prince Doran" :last_seen "Dorne"}
+               {:name "Tyrion Lannister" :status :alive :description "with Khaleesi. Advising" :last_seen "Mereen"}])
+
+(for [i lannbroh
+      j [{:type :noble :hometown "Casterly Rock"}]
+      :let [gabungan (merge i j)]]
+  gabungan)
+
